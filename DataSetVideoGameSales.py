9b7116edb7337fb8ab8bb6cd9e0a3fdf3b253bda
ethanshaw707 <ethanshaw707@gmail.com>
2025-01-13 12:19:59 -0700

    work in progress

--- DataSetVideoGameSales.py
+++ DataSetVideoGameSales.py
@@ -1,6 +1,1 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("willianoliveiragibin/video-game-sales-analyze")
-
-print("Path to dataset files:", path)
+print("Hello")
