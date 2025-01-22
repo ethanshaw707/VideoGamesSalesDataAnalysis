99b4ee0e89f4868dc004a65eeb522124eea513c6
JonnyAcosta <114012848+JonnyAcosta@users.noreply.github.com>
2025-01-22 10:31:20 -0700

    added a few comments

--- DataSetVideoGameSales.py
+++ DataSetVideoGameSales.py
@@ -17,16 +17,19 @@ print("\nDescriptive Statistics:")
 print(dataset.describe())
 
 # Code for the question: What are the ten 10 selling video games in North America and how much those ten games made in total global sales? 
+# This filter the dataset to remove the NA_sales that has 0 or doesn't have a value
 dataset_filter = dataset[dataset['NA_Sales'] > 0]
 
+#Sorts the dataset from top to bottom
 sorted_dataset = dataset_filter.sort_values(by='NA_Sales', ascending=False)
 
-Top_na_sales = dataset.sort_values(by="NA_Sales", ascending=False)
-
+#This sorts for only calling the top ten games in sells in NA
 top_10_games = sorted_dataset[['Name','Platform','NA_Sales', 'Global_Sales']].head(10)
 
+#This gets the sum of the total global sales of the top ten games
 global_sales = top_10_games['Global_Sales'].sum()
 
 print("Top 10 selling games in North America:")
 print(top_10_games)
+
 print(f"Total Global Sale of the ten selling games is {global_sales:.2f} million")
